fa3175b2f6d78d0e9e77c75eac258fe905075f21
Alejandro Luces Gonzalez <aluces@pcfactory.intranet>
2023-04-16 22:21:46 -0400

    Add folder JS and Style

---
+++ js/01.js
@@ -0,0 +1,7 @@
+// Con esto le di funcionalidad al icono para cerrar ventana
+const botonCerrar = document.getElementById("cerrar");
+botonCerrar.addEventListener("click", function() {window.close();
+});
+
+
+
